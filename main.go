114df0d0479f2e465ec321e6d41c0d41142ac4c8
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-28 16:02:08 +0200

    Updated CORS origin

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	cors := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
+		AllowedOrigins:   []string{"https://ibisubizo.com/*", "*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
